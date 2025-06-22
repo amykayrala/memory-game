60bcdd40dbcc00e54ead554efc20b20681670b0c
Amy Kayrala <amykayrala@gmail.com>
2025-06-21 20:09:51 -0400

    target emoji

--- src/Game.js
+++ src/Game.js
@@ -1,28 +1,60 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FiSettings } from 'react-icons/fi';
 
 const emojiThemes = { // Define color themes with arrays of colors
   Food: ['🍩', '🍔', '🍟', '🌭', '🍕', '🌮'], 
   Music: ['🎶', '🎧', '🎸', '🥁', '🎹', '🎷'],
 };
 
+function getRandomEmoji(emojis) { // Function to get a random color from the provided array
+  return emojis[Math.floor(Math.random() * emojis.length)];
+}
+
 function Game() {
-  const [score, setScore] = useState(0);
-    const [highscore, setHscore] = useState(() => {
-        const storedHscore = localStorage.getItem('highScore');
-        return storedHscore ? parseInt(storedHscore, 10) : 0;
-    });
   const location = useLocation();
   const { theme, difficulty } = location.state || {};
 
   const themeChosen = `${theme}-theme`;
    // const themeChosen = `${difficulty}-theme`;
+
+  const emojis = emojiThemes[theme]
+  const [targetEmoji, setTargetEmoji] = useState('null');
+
+  useEffect(() => {
+    setTargetEmoji(getRandomEmoji(emojis));;
+  }, []);
+
+
+  const [score, setScore] = useState(0);
+  const [highscore, setHscore] = useState(() => {
+      const storedHscore = localStorage.getItem('highScore');
+        return storedHscore ? parseInt(storedHscore, 10) : 0;
+  });
   
   return (
     <div className={`Game ${themeChosen}`}>
-        <h2 className="score">Score: {score}</h2>
-        <h2 className="highscore">High Score: {highscore}</h2>
+      <Container fluid>
+        <Row className="justify-content-md-center">
+          <Col className="text-start">
+            <h2 className="score">Score: {score}</h2>
+            <h2 className="highscore">High Score: {highscore}</h2>
+          </Col>
+          <Col className="text-center">
+            <div className='score' style={{fontSize: '3rem'}}> Catch the {targetEmoji} !</div>
+          </Col>
+          <Col className="text-end">
+            <FiSettings style={{fontSize: '2rem', marginLeft: '10px', marginTop: '10px'}}/>
+
+          </Col>
+        </Row>
+      </Container>
+        
+
 
     </div>
 
